72b57690ec329a4fe0e29a1484d031a0e788e138
Mikita Kryvitski <solairerove@gmail.com>
2019-05-01 15:55:40 +0300

    feature: set up new single view project

---
+++ ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  i-am-rich
+//
+//  Created by Nikita Krivitski on 5/1/19.
+//  Copyright © 2019 Nikita Krivitski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
